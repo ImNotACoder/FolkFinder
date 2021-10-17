f4dcb2fdf929cc0e793dcbd57ba69b209cc5315b
JorsinLuvhart <80300960+JorsinLuvhart@users.noreply.github.com>
2021-10-17 08:17:02 +0800

    Add files via upload

---
+++ rpiInterface.py
@@ -0,0 +1,40 @@
+import sys
+import time
+import random
+import telepot
+import RPi.GPIO as GPIO
+from telepot.loop import MessageLoop
+from gpiozero import Buzzer
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
+
+pwm = GPIO.PWM(16,1000)
+
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    
+    print('Got command: {0} from {1}'.format(command,chat_id))
+    
+    if command == '/triggerAlarm':
+        GPIO.output(18, GPIO.HIGH)
+        pwm.start(50)
+        bot.sendMessage(chat_id, 'Alarm has been triggered')
+        bot.sendMessage(27838129, 'Alarm has been triggered')
+    elif command == '/disableAlarm':
+        GPIO.output(18, GPIO.LOW)
+        pwm.stop()
+        bot.sendMessage(27838129, 'Alarm has been disabled')
+
+bot = telepot.Bot('2092432636:AAGdzPV2NYe0Jt5Jg9kI7XnwKm7kmWtqjUU')
+MessageLoop(bot,handle).run_as_thread()
+
+
+while True:
+    sleep(30)
+    
